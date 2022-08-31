e5ec479e94ddf5e1b757b8ccbd84976081a3ffe9
Christopher Garcia <garciawebdev4@gmail.com>
2022-08-31 16:35:51 -0400

    add the etch a sketch styling to it

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('button');
-const amtOfGrids = 16;
+const amtOfGrids = 100;
 
 const createGrid = (amtOfGrids) => {
     for(i = 0; i < amtOfGrids; i++){
@@ -9,6 +9,9 @@ const createGrid = (amtOfGrids) => {
     for(j = 0; j < amtOfGrids; j++){
         const box = document.createElement('div');
         box.classList.add('grid-box');
+        box.addEventListener('mouseenter', () => {
+            box.style.backgroundColor = 'black'
+        })
         row.appendChild(box);
         }
     container.appendChild(row);
