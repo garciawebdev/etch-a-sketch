8ac35c895913fd3d660b4ff78492b783db248c58
Christopher Garcia <garciawebdev4@gmail.com>
2022-09-06 17:21:53 -0400

    fixing detached head

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('button');
-const amtOfGrids = 100;
+let amtOfGrids;
+
+const sqaure = () => {
+    let grid = Number(prompt('Choose a number under 100'));
+    if(grid > 100){
+        sqaure()
+    } else if (grid < 100){
+        amtOfGrids = grid
+        createGrid(amtOfGrids);
+    }
+}
+
 
 const createGrid = (amtOfGrids) => {
     for(i = 0; i < amtOfGrids; i++){
@@ -8,7 +19,10 @@ const createGrid = (amtOfGrids) => {
         row.classList.add('grid-row');
     for(j = 0; j < amtOfGrids; j++){
         const box = document.createElement('div');
+        const widthHeight = 980 / amtOfGrids
         box.classList.add('grid-box');
+        box.style.width = `${widthHeight}px`
+        box.style.height = `${widthHeight}px`
         box.addEventListener('mouseenter', () => {
             box.style.backgroundColor = 'black'
         })
@@ -18,4 +32,16 @@ const createGrid = (amtOfGrids) => {
     }
 }
 
-createGrid(amtOfGrids);
+
+
+sqaure();
+
+
+
+resetButton.addEventListener('click', ()=> {
+    const gridRow = document.querySelectorAll('.grid-row');
+    gridRow.forEach(div => {
+        div.remove();
+    })
+    sqaure();
+})
